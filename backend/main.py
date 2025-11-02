bac987e165c958bf90f725b839aff22721a542b3
Kapitar <kkapitar@ya.ru>
2025-11-02 15:56:33 -0500

    feat: add nice handling of rate limit

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import os
+import math
+import time
 import json
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
@@ -45,7 +47,7 @@ async def process_github_events():
             event_data = json.loads(event.decode('utf-8'))
             has_force_push = await is_force_push(event_data["repo"]["name"], event_data["payload"]["before"], event_data["payload"]["head"])
             
-            print(event_data["repo"]["name"], has_force_push)
+            # print(event_data["repo"]["name"], has_force_push)
             
             await asyncio.sleep(0.1)
         except Exception as e:
@@ -54,24 +56,75 @@ async def process_github_events():
 
 
 async def poll_github_events():
+    ETag = None
+    poll_interval = 15
+    attempts = 0
+    max_retries = 10
+    base_delay = 60.0 # 1 minute
+    max_delay = 15 * 60.0 # 15 minutes
+    
     async with httpx.AsyncClient() as client:
         while True:
             try:
+                headers = {
+                    "Accept": "application/vnd.github+json",
+                    "X-GitHub-Api-Version": "2022-11-28",
+                    "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"
+                }
+                if ETag:
+                    headers["If-None-Match"] = ETag
+                
                 response = await client.get(
                     "https://api.github.com/events",
-                    headers = {
-                        "Accept": "application/vnd.github+json",
-                        "X-GitHub-Api-Version": "2022-11-28",
-                        "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"
-                    }
                 )
+                
+                if response.status_code in (403, 429):
+                    retry_after = response.headers.get("retry-after")
+                    if retry_after:
+                        delay = int(math.ceil(float(retry_after)))
+                        await asyncio.sleep(max(1, delay))
+                        attempts += 1
+                        if attempts > max_retries:
+                            response.raise_for_status()
+                        continue
+                    
+                    ratelimit_remaining = response.headers.get("x-ratelimit-remaining")
+                    ratelimit_reset = response.headers.get("x-ratelimit-reset")
+                    if ratelimit_remaining == "0" and ratelimit_reset:
+                        try:
+                            reset_epoch = int(ratelimit_reset)
+                            now = time.time()
+                            delay = max(0, reset_epoch - now)
+                            await asyncio.sleep(delay)
+
+                            attempts += 1
+                            if attempts > max_retries:
+                                response.raise_for_status()
+                            continue
+                        except ValueError:
+                            print("Error parsing rate limit reset time")
+                            pass
+                    
+                    if attempts >= max_retries:
+                        response.raise_for_status()
+                        continue
+
+                    delay = min(max_delay, base_delay * (2 ** attempts))
+                    await asyncio.sleep(delay) 
+                    attempts += 1
+                    continue
+
+                if response.status_code == 304:
+                    poll_interval = int(response.headers.get("X-Poll-Interval", poll_interval))
+                    await asyncio.sleep(poll_interval)
+                    continue
                 
                 events = response.json()
                 for event in events:
                     if event["type"] == "PushEvent":
                         await redis.rpush("push_events", json.dumps(event))
                     
-                await asyncio.sleep(15)
+                await asyncio.sleep(poll_interval)
             except Exception as e:
                 print("Error polling GitHub events:", e)
     
